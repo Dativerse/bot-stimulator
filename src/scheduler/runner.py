2c7a4937736650418f3e359cf68d0d2859e8fdb0
Loki.nguyen <loki.nguyen@sabawork.com>
2026-06-25 17:40:48 +0700

    feat: make runner run imediatly after re-deploy

--- src/scheduler/runner.py
+++ src/scheduler/runner.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.triggers.cron import CronTrigger
+from datetime import datetime
 
 def _run_scheduler_safe(scheduler: BlockingScheduler):
     try:
@@ -14,9 +15,15 @@ def start_cron_job(job_func, cron_schedule, id='bot-stimulator-sync', replace_ex
     scheduler = BlockingScheduler()
     trigger = CronTrigger.from_crontab(cron_schedule)
     
-    # Add the job to run based on the schedule
-    scheduler.add_job(job_func, trigger, id=id, replace_existing=replace_existing)
+    # Add the job to run based on the schedule, and set it to run immediately
+    scheduler.add_job(
+        job_func, 
+        trigger, 
+        id=id, 
+        replace_existing=replace_existing,
+        next_run_time=datetime.now()
+    )
     
-    print("Scheduler started. Task will run based on the provided trigger.")
+    print("Scheduler started. Task will run immediately, and then based on the provided trigger.")
     print("Press Ctrl+C to exit.")
     _run_scheduler_safe(scheduler)
